85d8adb382c132e6e56110668b72720db669b145
Shubham Padia <shubhamapadia@gmail.com>
2024-03-05 18:14:50 +0530

    Common: respond with pong as a welcome message

--- app/main.js
+++ app/main.js
@@ -6,5 +6,12 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((connection) => {
   // Handle connection
 });
+server.on("connection", (socket) => { 
+    
+    socket.on("data", () => { 
+        socket.write("+PONG\r\n");
+    });
+
+  }); 
 
 server.listen(6379, "127.0.0.1");
